aa49a4d87932f10f57f5193391717a5e7314345a
Skyler A Sidner <skylerasidner@Skylers-MacBook-Pro.local>
2017-08-28 16:51:15 -0500

    broken apart components; still needs webpack

---
+++ components/menu.js
@@ -0,0 +1,15 @@
+import Recipe from './recipe'
+
+const Menu = ({ recipes }) =>
+    <article>
+        <header>
+            <h1>Delicious Recipes</h1>
+        </header>
+        <div className="recipes">
+            { recipes.map((recipe, i) =>
+                <Recipe key={i} {...recipe} />)
+            }
+        </div>
+    </article>
+
+export default Menu
---
+++ components/recipe.js
@@ -0,0 +1,12 @@
+import IngredientsList from './ingredientsList'
+import Instructions from './instructions'
+
+const Recipe = ({name, ingredients, steps}) => (
+    <section id={name.toLowerCase().replace(/ /g, "-")}>
+        <h1>{name}</h1>
+        <IngredientsList list={...ingredients} />
+        <Instructions title="Cooking Instructions" {...steps} />
+    </section>
+)
+
+export default Recipe
